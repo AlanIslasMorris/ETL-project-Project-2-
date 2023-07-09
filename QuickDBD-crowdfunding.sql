3236978e11eca670688f29d54d945682f528eb35
AlanIslasMorris <70079035+AlanIslasMorris@users.noreply.github.com>
2023-07-08 21:50:42 -0600

    Add files via upload
    The tables were uploaded to pgAdmin, using select it was verified that the data was loaded successfully

---
+++ QuickDBD-crowdfunding.sql
@@ -0,0 +1,60 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/9SNscs
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "subcategory" (
+    "subcategory_id" varchar(10)   NOT NULL,
+    "subcategory" varchar(30)   NOT NULL,
+    CONSTRAINT "pk_subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+CREATE TABLE "contact" (
+    "contact_id" int   NOT NULL,
+    "first_name" varchar(30)   NOT NULL,
+    "last_name" varchar(30)   NOT NULL,
+    "email" varchar(50)   NOT NULL,
+    CONSTRAINT "pk_contact" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+CREATE TABLE "category" (
+    "category_id" varchar(10)   NOT NULL,
+    "category" varchar(30)   NOT NULL,
+    CONSTRAINT "pk_category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "crowdfunding" (
+    "cf_id" int   NOT NULL,
+    "contact_id" int   NOT NULL,
+    "company_name" varchar(40)   NOT NULL,
+    "description" varchar(100)   NOT NULL,
+    "goal" float   NOT NULL,
+    "pledged" float   NOT NULL,
+    "outcome" varchar(20)   NOT NULL,
+    "backers_count" int   NOT NULL,
+    "country" varchar(10)   NOT NULL,
+    "currency" varchar(10)   NOT NULL,
+    "launched_date" date   NOT NULL,
+    "end_date" date   NOT NULL,
+    "category_id" varchar(10)   NOT NULL,
+    "subcategory_id" varchar(10)   NOT NULL,
+    CONSTRAINT "pk_crowdfunding" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+ALTER TABLE "crowdfunding" ADD CONSTRAINT "fk_crowdfunding_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "contact" ("contact_id");
+
+ALTER TABLE "crowdfunding" ADD CONSTRAINT "fk_crowdfunding_category_id" FOREIGN KEY("category_id")
+REFERENCES "category" ("category_id");
+
+ALTER TABLE "crowdfunding" ADD CONSTRAINT "fk_crowdfunding_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "subcategory" ("subcategory_id");
+
